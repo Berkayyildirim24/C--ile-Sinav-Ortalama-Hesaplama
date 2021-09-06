686b8225325aebbce35847c6899b4a7a6e6e50c1
Berkayyildirim24 <90174527+Berkayyildirim24@users.noreply.github.com>
2021-09-06 22:05:19 +0300

    C# ile Sınav Ortalama Hesaplama
    C# form uygulamasıyla yapmış olduğum öğrenci sınav ortalamasını alan program.

---
+++ Sınav Ortalama/Sınav Ortalama/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Sınav_Ortalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.l1 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            this.li1 = new System.Windows.Forms.ListBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.l1.Location = new System.Drawing.Point(57, 42);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(40, 22);
+            this.l1.TabIndex = 0;
+            this.l1.Text = "AD:";
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.l2.Location = new System.Drawing.Point(56, 110);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(78, 22);
+            this.l2.TabIndex = 1;
+            this.l2.Text = "SOYAD:";
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.l3.Location = new System.Drawing.Point(322, 42);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(78, 22);
+            this.l3.TabIndex = 3;
+            this.l3.Text = "SINAV1:";
+            // 
+            // l4
+            // 
+            this.l4.AutoSize = true;
+            this.l4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.l4.Location = new System.Drawing.Point(322, 110);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(70, 22);
+            this.l4.TabIndex = 2;
+            this.l4.Text = "Sınav2:";
+            // 
+            // l5
+            // 
+            this.l5.AutoSize = true;
+            this.l5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.l5.Location = new System.Drawing.Point(57, 194);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(57, 22);
+            this.l5.TabIndex = 5;
+            this.l5.Text = "Proje:";
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(346, 186);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(186, 38);
+            this.b1.TabIndex = 11;
+            this.b1.Text = "HESAPLA";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.Hesapla_Click);
+            // 
+            // li1
+            // 
+            this.li1.FormattingEnabled = true;
+            this.li1.Location = new System.Drawing.Point(60, 287);
+            this.li1.Name = "li1";
+            this.li1.Size = new System.Drawing.Size(679, 147);
+            this.li1.TabIndex = 12;
+            // 
+            // pb1
+            // 
+            this.pb1.Image = ((System.Drawing.Image)(resources.GetObject("pb1.Image")));
+            this.pb1.Location = new System.Drawing.Point(580, 22);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(168, 110);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 13;
+            this.pb1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(432, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(432, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(167, 196);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.li1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l1);
+            this.Name = "Form1";
+            this.Text = "Sınav Ortalama";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.ListBox li1;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
